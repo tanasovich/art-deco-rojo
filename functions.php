5d9d06c467837665bf97b5fde5cd7b0e56382c9e
Valentine Tanasovich <valentine.tanasovich@gmail.com>
2024-11-29 01:01:16 +0200

    Page with teapot slug is special
    It has HTTP 418 response code, because we like a tea, but this
    page doesn't cook it.

--- functions.php
+++ functions.php
@@ -8,3 +8,13 @@ function seleni_enqueue_styles() {
 }
 
 add_action( 'wp_enqueue_scripts', 'seleni_enqueue_styles' );
+
+function handle_teapot() {
+	if ( is_page('teapot') ) {
+		status_header( 418 );
+		nocache_headers();
+	}
+}
+
+add_action( 'pre_get_posts', 'handle_teapot' );
+
